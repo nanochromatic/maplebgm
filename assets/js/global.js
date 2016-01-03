2cdad753baa48463a10bf12fc8044b37c29276b2
Wesley Dai <elinforest@gmail.com>
2016-01-03 15:34:50 -0500

    open new tab for external links

---
+++ assets/js/global.js
@@ -0,0 +1,18 @@
+(function($){
+	// Open external links in new window
+	var externalLinks = function(){
+		var host = location.host;
+
+		$('body').on('click', 'a', function(e){
+			var href = this.href,
+				link = href.replace(/https?:\/\/([^\/]+)(.*)/, '$1');
+
+			if (link != '' && link != host && !$(this).hasClass('fancybox')){
+				window.open(href);
+				e.preventDefault();
+			}
+		});
+	};
+
+	externalLinks(); // Delete or comment this line to disable opening external links in new window
+})(jQuery);
